7640131e34c6c578c20c22025eb04ba9f69933c7
rohitkumar-bca <gamingislove2020@gmail.com>
2025-06-29 13:13:35 +0530

    Add files via upload

---
+++ OR code generator/index.py
@@ -0,0 +1,11 @@
+# import qrcode as qr
+# img = qr.make("https://youtube.com/@chandan_eisha?si=FKtiXpv-L3juccR0")
+# img.save("youtube_qr.png")
+
+import qrcode
+from PIL import Image
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10, border=5)
+qr.add_data("https://youtube.com/@chandan_eisha?si=FKtiXpv-L3juccR0")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save("qr.png")
